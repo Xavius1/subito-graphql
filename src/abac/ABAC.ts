3a2e6cb6502672840514ee32c880cc5e59ab3450
Xavier <xherriot@protonmail.com>
2022-06-30 18:40:11 +0200

    feat(Abac): optim class

--- src/abac/ABAC.ts
+++ src/abac/ABAC.ts
@@ -5,7 +5,7 @@
  * @example
  * ```
  * class MyAbac extends Abac {
- *   public Entity: EntityAbac
+ *   protected policies = [ EntityPolicy ];
  * }
  * ```
  * Then into index.ts
@@ -14,17 +14,32 @@
  *   abac: new MyAbac()
  * })
  * ```
+ * To use it:
+ * ```
+ * const { dataSources: { Abac } } = context;
+ * Abac.EntityPolicy.create(input);
+ * Abac.EntityPolicy.delete(input);
+ * Abac.EntityPolicy.read(doc);
+ * Abac.EntityPolicy.update(input);
+ * ```
  *
  * @public
  */
 abstract class Abac {
   protected context = null;
 
-  initialize({
+  protected policies = [];
+
+  public initialize({
     // @ts-ignore TODO need to be handle
     context,
   } = {}) {
     this.context = context;
+    const { policies } = this;
+    Object.keys(policies).forEach((policy: string) => {
+      // @ts-ignore TODO need to be handle
+      this[policy] = new policies[policy](context);
+    });
   }
 }
 
