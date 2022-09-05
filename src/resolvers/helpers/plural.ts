d6444901d6c53b76afb24ef2bc3595457fbc7234
Xavius <xavius@tutanota.com>
2022-09-05 16:04:17 +0200

    feat(plural): add unit test

--- src/resolvers/helpers/plural.ts
+++ src/resolvers/helpers/plural.ts
@@ -1,3 +1,11 @@
+/**
+ * Transform a string to its plural
+ *
+ * @param str - The input string
+ * @returns
+ *
+ * @public
+ */
 const plural = (str: string): string => {
   if (str.substring(str.length - 1) === 'y') {
     return `${str.substring(0, str.length - 1)}ies`;
